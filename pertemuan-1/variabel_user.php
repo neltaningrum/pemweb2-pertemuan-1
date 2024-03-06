4c376f4252d93872a85317b3b4faa268dedd71bf
neltaningrum <147047845+neltaningrum@users.noreply.github.com>
2024-03-06 21:19:17 +0700

    Add files via upload

---
+++ pertemuan-1/variabel_user.php
@@ -0,0 +1,10 @@
+<?php
+$nama = "Nelta Ningrum Br Nasution";
+$umur = 18;
+$berat = 59;
+
+echo "Nama : ' . $nama";
+echo "<br/>Umur : $umur Tahun";
+echo "<br/>Berat : $berat Kg";
+
+echo "<br/>Hello $nama Apakabar";
